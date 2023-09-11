c9499856138d184e1602af6752c3a73cee053910
Md Bappy <mdbappy1350@gmail.com>
2023-09-11 20:59:16 +0600

    Starting Cpp

---
+++ Practice/user_input.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter an interger : ";
+    cin >> num;
+    cout<<"The number is : "<<num<<endl;
+
+    float num1;
+    cout<<"Enter a floting Number : ";
+    cin >> num1;
+    cout<<"The number is : "<<num1<<endl;
+
+    double num2;
+    cout<<"Enter a double Number : ";
+    cin >> num2;
+    cout<<"The number is : "<<num2<<endl;
+
+    char num3;
+    cout<<"Enter a Character : ";
+    cin >> num3;
+    cout<<"The Character is : "<<num3<<endl;
+
+    string name;
+    cout<<"Enter your Name : ";
+    cin>>name;
+    cout<<"Your  name is : "<<name<<endl;
+
+    char name1[20];
+    cout<<"Enter your Name : ";
+    cin>>name1;
+    cout<<"Your  name is : "<<name1<<endl;
+}
