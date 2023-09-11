136661b5aa2b268a81db52f52f7577b2b3a7ac2c
Md Bappy <mdbappy21.cse@gmail.com>
2023-09-11 20:59:16 +0600

    Starting Cpp

---
+++ Practice/EscapeSequence.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello Everyone, " <<endl<<"I am Bappy"<<endl;
+    cout<<"Contact\t: "<<"mdbappy1350@gmail.com"<<endl;
+    cout<<"Address\t: "<<"Pabna\n";
+}
